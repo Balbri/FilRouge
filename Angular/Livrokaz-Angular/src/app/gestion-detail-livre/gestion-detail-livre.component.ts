921e8f45ead485de02a8ae3b5d39fe6ece861cf8
CedricT33 <cedric.tressous@gmail.com>
2019-02-13 23:02:01 +0100

    Ajout theme perso + amelioration formulaire gestion-detail-livre

--- Angular/Livrokaz-Angular/src/app/gestion-detail-livre/gestion-detail-livre.component.ts
+++ Angular/Livrokaz-Angular/src/app/gestion-detail-livre/gestion-detail-livre.component.ts
@@ -5,6 +5,8 @@ import { Langue } from '../Model/langue';
 import { Editeur } from '../Model/editeur';
 import { Genre } from '../Model/genre';
 import { Location } from '@angular/common';
+import { Livre } from '../Model/livre';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-gestion-detail-livre',
@@ -18,15 +20,37 @@ export class GestionDetailLivreComponent implements OnInit {
   editeurs: Editeur[] = [];
   genres: Genre[] = [];
   imageUrl = '';
+  leLivre: Livre;
+  livreForm: FormGroup;
 
   constructor(private datasService: DatasService,
-              private location: Location) { }
+              private location: Location,
+              private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.getAuteurs();
     this.getLangues();
     this.getEditeurs();
     this.getGenres();
+    this.initForm();
+  }
+
+  initForm() {
+    this.livreForm = this.formBuilder.group({
+      isbn: ['', Validators.required],
+      titreLivre: ['', Validators.required],
+      imageCouverture: ['', Validators.required],
+      anneeParution: ['', Validators.required],
+      stock: ['', Validators.required],
+      prixNeuf: ['', Validators.required],
+      prixOccas: ['', Validators.required],
+      langue: [null, Validators.required],
+      editeur: [null, Validators.required],
+      auteurs: this.formBuilder.array([]),
+      genres: this.formBuilder.array([]),
+      sujetLivre: ['', Validators.required],
+      descriptionLivre: ['', Validators.required]
+    });
   }
 
   getLangues() {
@@ -46,6 +70,44 @@ export class GestionDetailLivreComponent implements OnInit {
   }
 
   onSave() {
+    const formValue = this.livreForm.value;
+    const newLivre = new Livre(
+      5,
+      formValue['isbn'].toString(),
+      formValue['titreLivre'],
+      formValue['imageCouverture'],
+      formValue['sujetLivre'],
+      formValue['descriptionLivre'],
+      formValue['anneeParution'],
+      +formValue['prixOccas'],
+      +formValue['prixNeuf'],
+      formValue['langue'],
+      formValue['editeur'],
+      +formValue['stock'],
+      formValue['genres'] ? formValue['genres'] : [],
+      formValue['auteurs'] ? formValue['auteurs'] : []
+    );
+    this.datasService.createLivre(newLivre);
+    console.log(newLivre);
+    this.location.back();
+  }
+
+  getAuteursControl() {
+    return this.livreForm.get('auteurs') as FormArray;
+  }
+
+  onAddAuteur() {
+    const newAuteurControl = this.formBuilder.control('', Validators.required);
+    this.getAuteursControl().push(newAuteurControl);
+  }
+
+  getGenresControl() {
+    return this.livreForm.get('genres') as FormArray;
+  }
+
+  onAddGenre() {
+    const newGenreControl = this.formBuilder.control('', Validators.required);
+    this.getGenresControl().push(newGenreControl);
   }
 
   onBack() {
