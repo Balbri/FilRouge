5d4b894b0e516ef8b6954ba693fe8f0d7c5363dd
CedricT33 <cedric.tressous@gmail.com>
2019-02-24 11:10:03 +0100

    Update HTML et CSS gestion-detail-user

--- Angular/Livrokaz-Angular/src/app/app.module.ts
+++ Angular/Livrokaz-Angular/src/app/app.module.ts
@@ -1,28 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './Angular-Material';
 import { HeaderComponent } from './header/header.component';
 import { PubComponent } from './pub/pub.component';
 import { ListeComponent } from './liste/liste.component';
-import { HttpClientModule } from '@angular/common/http';
 import { LivreDetailComponent } from './livre-detail/livre-detail.component';
 import { GestionLivresComponent } from './gestion-livres/gestion-livres.component';
 import { GestionDetailLivreComponent } from './gestion-detail-livre/gestion-detail-livre.component';
-import { LivresService } from './services/livres.service';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { GestionLanguesComponent } from './gestion-langues/gestion-langues.component';
-import { LanguesService } from './services/langues.service';
 import { GestionDetailLangueComponent } from './gestion-detail-langue/gestion-detail-langue.component';
 import { GestionGenresComponent } from './gestion-genres/gestion-genres.component';
 import { GestionDetailGenreComponent } from './gestion-detail-genre/gestion-detail-genre.component';
-import { GenresService } from './services/genres.service';
-import { UsersService } from './services/users.service';
 import { GestionUsersComponent } from './gestion-users/gestion-users.component';
 import { GestionDetailUserComponent } from './gestion-detail-user/gestion-detail-user.component';
-import { AuthoritiesService } from './services/authorities.service';
 
 @NgModule({
   declarations: [
@@ -49,7 +45,7 @@ import { AuthoritiesService } from './services/authorities.service';
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [LivresService, LanguesService, GenresService, UsersService, AuthoritiesService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
